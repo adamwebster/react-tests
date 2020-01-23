c944064f84686f983ab69adf8c2bab21f101a51b
Adam Webster <awebster@bnasdev.onmicrosoft.com>
2020-01-23 08:28:55 -0500

    Simple layout css grid

--- src/pages/CSSGrid.js
+++ src/pages/CSSGrid.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
 const Grid = styled.div`
   display:grid;
   grid-gap: 10px;
   grid-template-columns: auto auto auto auto auto auto auto auto auto auto auto auto;
+  grid-template-rows: 50px 200px 200px 50px;
   background-color: tomato;
   padding:10px;
 `
@@ -13,6 +14,13 @@ const GridItem = styled.div`
   background-color: #fff;
   border: solid 1px #ccc;
   padding:20px;
+  ${props => props.gridColumn && css`
+    grid-column: ${props.gridColumn};
+  `}
+
+  ${props => props.gridRow && css`
+    grid-row: ${props.gridRow};
+  `}
 `
 const CSSGrid = () => {
   const numberOfItems = 9;
@@ -28,30 +36,12 @@ const CSSGrid = () => {
   return (
     <Grid>
       {/* {createGridItem()} */}
-      <GridItem />
-      <GridItem />
-      <GridItem />
-      <GridItem />
-      <GridItem />
-      <GridItem />
-      <GridItem />
-      <GridItem />
-      <GridItem />
-      <GridItem />
-      <GridItem />
-      <GridItem />
-      <GridItem />
-      <GridItem />
-      <GridItem />
-      <GridItem />
-      <GridItem />
-      <GridItem />
-      <GridItem />
-      <GridItem />
-      <GridItem />
-      <GridItem />
-      <GridItem />
-      <GridItem />
+      <GridItem gridColumn={"1 / 13"} />
+
+      <GridItem gridColumn={"1 / 3"} gridRow={"2 / span 3"}  />
+      <GridItem  gridColumn={"3 / 13"}  gridRow={"2 / span 3"} />
+      <GridItem gridColumn={"1 / 13"} />
+
     </Grid>
   )
 };
