b1f222522ce4ad72dd42d5e9540147525f196fc4
Adam Webster <adamwebster@me.com>
2020-01-30 20:59:30 -0500

    Working on News Feed Page

--- src/pages/NewsFeed.js
+++ src/pages/NewsFeed.js
@@ -1,42 +1,74 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import styled from "styled-components";
 import PropTypes from "prop-types";
-import { Card } from "@adamwebster/fused-components";
+import {
+  Card,
+  AnimatedButton,
+  Avatar,
+  Label
+} from "@adamwebster/fused-components";
 import FeedData from "../data/Feed.json";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { darken } from "polished";
 
-const Feed = styled(Card)`
+const Feed = styled.div`
   width: 600px;
   margin: 40px auto;
-  overflow:hidden;
-`;
-const FeedItem = styled.div`
-  width: 100%;
-  box-sizing: border-box;
 `;
 
-const Username = styled.div`
+const FeedItem = styled(Card)`
+  width: 100%;
+  box-sizing: border-box;
+  margin-bottom: 10px;
+  overflow: hidden;
+`;
+
+const Profile = styled.div`
+  padding: 10px 10px 0 10px;
+  display: flex;
+  flex: 1 1;
+  border-bottom: solid 1px #dcdcdc;
+  margin-bottom: 10px;
+`;
+
+const UserName = styled.div`
   font-weight: bold;
   margin-bottom: 10px;
   font-size: 16px;
-  color: #3db3ee;
-  padding: 10px 10px 0 10px;
+  padding:20px 0px;
+`
+
+const AvatarStyled = styled(Avatar)`
+  border: solid 3px #fff;
+  display: inline-block;
+  margin-right: 20px;
+`;
+
+const NewPost = styled.div`
+  box-sizing: border-box;
+  margin-bottom: 20px;
+  textarea {
+    width: 100%;
+    border-radius: 5px;
+    resize: vertical;
+    border: solid 1px #ccc;
+    box-sizing: border-box;
+    min-height: 100px;
+  }
 `;
 
 const Content = styled.div`
   padding: 10px;
-`
+`;
 const Meta = styled.div`
   font-size: 14px;
   margin-top: 10px;
   padding: 10px;
   box-sizing: border-box;
   background-color: #f2f2f2;
-`
-const Date = styled.div`
+`;
+const DateValue = styled.div`
   color: #939393;
-
 `;
 
 const Actions = styled.div`
@@ -45,18 +77,18 @@ const Actions = styled.div`
   svg:not(:last-child) {
     margin-right: 10px;
   }
-  .fa-smile-wink{
-      color: #ff9b00;
-      cursor:pointer;
+  .fa-smile-wink {
+    color: #ff9b00;
+    cursor: pointer;
     transition: all 0.2s ease;
     &:hover {
       transform: scale(1.2);
       color: ${darken(0.1, "#ff9b00")};
     }
   }
-  .fa-comment{
+  .fa-comment {
     color: #3db3ee;
-    cursor:pointer;
+    cursor: pointer;
     transition: all 0.2s ease;
     &:hover {
       transform: scale(1.2);
@@ -65,7 +97,7 @@ const Actions = styled.div`
   }
   .fa-heart {
     color: red;
-    cursor:pointer;
+    cursor: pointer;
     transition: all 0.2s ease;
     &:hover {
       transform: scale(1.2);
@@ -74,29 +106,60 @@ const Actions = styled.div`
   }
 `;
 const NewsFeed = () => {
-  const { FeedItems, setFeedItems } = useState(FeedData);
-  console.log(FeedData);
+  const [FeedItems, setFeedItems] = useState(FeedData);
+  const [FeedText, setFeedText] = useState("");
+  const addFeedItem = () => {
+    const FeedItemCopy = FeedItems.slice();
+    const d = new Date();
+    FeedItemCopy.unshift({
+      id: FeedItemCopy.length + 1,
+      user: "Adam Webster",
+      avatar: "https://adamwebster.me/static/avatar.jpg",
+      date: d.toString(),
+      content: FeedText
+    });
+    setFeedItems(FeedItemCopy);
+  };
   return (
-    <Feed boxShadow>
-      {FeedData.map(item => {
-        return (
-          <FeedItem>
-            <Username>{item.user}</Username>
-            <Content>{item.content}</Content>
-            <Meta>
-            <Date>
-              {item.date}
-              <Actions>
-                <FontAwesomeIcon icon="heart" />
-                <FontAwesomeIcon icon="comment" />
+    <Feed>
+      <NewPost>
+        <Label htmlFor="NewPost">New Post</Label>
+        <textarea onChange={e => setFeedText(e.target.value)} id="NewPost" />
+        <AnimatedButton
+          icon={<FontAwesomeIcon icon="plus" />}
+          onClick={() => addFeedItem()}
+          primary
+        >
+          Post
+        </AnimatedButton>
+      </NewPost>
+      {FeedItems &&
+        FeedItems.map(item => {
+          return (
+            <FeedItem boxShadow key={item.id}>
+              <Profile>
+                <AvatarStyled
+                  boxShadow
+                  borderRadius="50px"
+                  image={item.avatar}
+                />
+                <UserName>{item.user}</UserName>
+              </Profile>
+              <Content>{item.content}</Content>
+              <Meta>
+                <DateValue>
+                  {item.date}
+                  <Actions>
+                    <FontAwesomeIcon icon="heart" />
+                    <FontAwesomeIcon icon="comment" />
 
-                <FontAwesomeIcon icon={["fas", "smile-wink"]} />
-              </Actions>
-            </Date>
-            </Meta>
-          </FeedItem>
-        );
-      })}
+                    <FontAwesomeIcon icon={["fas", "smile-wink"]} />
+                  </Actions>
+                </DateValue>
+              </Meta>
+            </FeedItem>
+          );
+        })}
     </Feed>
   );
 };
