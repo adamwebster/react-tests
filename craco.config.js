f49ec4a6a892253a526ffd18d214f42d0b787f60
Adam Webster <adamwebster@me.com>
2020-02-22 12:41:34 -0500

    Updating this to use Craco for linking the components

---
+++ craco.config.js
@@ -0,0 +1,9 @@
+const path = require('path')
+// Fix issue with multiple versions for multiple versions of react when linking the FusedComponents project locally
+module.exports = {
+  webpack: {
+    alias: {
+      react: path.resolve(__dirname, './node_modules/react'),
+    },
+  },
+}
