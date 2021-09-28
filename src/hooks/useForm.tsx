a28dc54bf3c90501e33de2f037caf69acd11ef90
Adam Webster <adamwebster@me.com>
2021-09-28 13:03:24 -0400

    Added useForm

--- src/hooks/useForm.tsx
+++ src/hooks/useForm.tsx
@@ -1,7 +1,9 @@
+import { Alert } from '@adamwebster/fused-components';
 import { ChangeEvent, FormEvent, useState } from 'react';
 
-export const useForm = ({ initialValues, onSubmit }: any) => {
+export const useForm = ({ initialValues, onSubmit, validations = {} }: any) => {
     const [data, setData] = useState(initialValues || {});
+    const [errors, setErrors] = useState<any>([]);
 
     const getValue = (element: ChangeEvent<HTMLInputElement>) => {
         switch (element.target.type) {
@@ -21,12 +23,60 @@ export const useForm = ({ initialValues, onSubmit }: any) => {
         });
     };
 
+    const ErrorComponent = () => {
+        if (errors.length > 0)
+            return (
+                <Alert fcStyle="danger">
+                    <ul>
+                        {errors.map((error: any) => (
+                            <li>{error.message}</li>
+                        ))}
+                    </ul>
+                </Alert>
+            );
+        return <></>;
+    };
+
     const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
         e.preventDefault();
+        if (validations) {
+            let valid = true;
+            const newErrors: any = [];
+            for (const key in validations) {
+                const value = data[key];
+                const validation = validations[key];
+                if (validation?.required?.value && !value) {
+                    valid = false;
+                    newErrors.push({
+                        key,
+                        message: validation?.required?.message,
+                    });
+                }
+
+                const pattern = validation?.pattern;
+                if (pattern?.value && !RegExp(pattern.value).test(value)) {
+                    valid = false;
+                    newErrors.push({ key, message: pattern.message });
+                }
+
+                const custom = validation?.custom;
+                if (custom?.isValid && !custom.isValid(value)) {
+                    valid = false;
+                    newErrors.push({ key, message: custom.message });
+                }
+            }
+
+            if (!valid) {
+                setErrors(newErrors);
+                return;
+            }
+        }
+
+        setErrors({});
         if (onSubmit) {
             onSubmit();
         }
     };
 
-    return { data, handleChange, handleSubmit };
+    return { data, handleChange, handleSubmit, errors, ErrorComponent };
 };
--- src/pages/UseFormPage/UseFormPage.tsx
+++ src/pages/UseFormPage/UseFormPage.tsx
@@ -1,14 +1,20 @@
 import {
     Button,
-    Checkbox,
     Input,
     Label,
     FormField,
+    Alert,
 } from '@adamwebster/fused-components';
 import { useForm } from '../../hooks/useForm';
 
 const UseFormPage = () => {
-    const { data, handleChange, handleSubmit } = useForm({
+    const {
+        data,
+        handleChange,
+        handleSubmit,
+        errors,
+        ErrorComponent,
+    } = useForm({
         initialValues: {
             username: 'John',
             password: '',
@@ -17,19 +23,58 @@ const UseFormPage = () => {
             radios: 'one',
         },
         onSubmit: () => console.log('submit', data),
+        validations: {
+            password: {
+                required: {
+                    value: true,
+                    message: 'This password field is required',
+                },
+            },
+            username: {
+                required: {
+                    value: true,
+                    message: 'This username field is required',
+                },
+            },
+            rememberMe: {
+                required: {
+                    value: true,
+                    message: 'This remember me field is required',
+                },
+            },
+        },
     });
+
+    const checkIfInError = (key: string) => {
+        return (
+            errors.filter(
+                (error: { key: string; message: string }) => error.key === key
+            ).length > 0
+        );
+    };
     return (
         <>
+            <ErrorComponent />
             <form onSubmit={handleSubmit}>
-                <FormField label="Name" htmlFor="name">
+                <FormField
+                    label="Name"
+                    htmlFor="name"
+                    validationMessage={errors.username}
+                >
                     <Input
+                        inError={checkIfInError('username')}
                         id="name"
                         value={data.username || ''}
                         onChange={handleChange('username')}
                     />
                 </FormField>
-                <FormField label="Password" htmlFor="Password">
+                <FormField
+                    label="Password"
+                    htmlFor="Password"
+                    validationMessage={errors.password}
+                >
                     <Input
+                        inError={checkIfInError('password')}
                         type="password"
                         id="password"
                         value={data.password || ''}
